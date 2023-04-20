4e897d1d9f2db00d731611c9f20d98786863bbd9
Matteo Antunez <antunezmatteo@gmail.com>
2023-04-20 09:24:53 -0500

    Update SGB.sql

---
+++ SGB.sql
@@ -0,0 +1,45 @@
+DROP DATABASE SGB;
+
+CREATE DATABASE SGB;
+
+USE SGB;
+
+CREATE TABLE teacher (
+	teacher_id int NOT NULL AUTO_INCREMENT,
+    teacher_firstName VARCHAR(50),
+	teacher_lastName VARCHAR(50),
+    teacher_username varchar(100),
+    teacher_password varchar(100),
+    Primary Key (teacher_id)
+);
+
+CREATE TABLE class (
+	class_id int NOT NULL AUTO_INCREMENT,
+    class_name varchar(100),
+    Primary Key (class_id)
+);
+
+CREATE TABLE student (
+	student_id int NOT NULL AUTO_INCREMENT,
+    student_firstName varchar(50),
+    student_lastName varchar(50),
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE teacher_class (
+	teacher_id int NOT NULL,
+    class_id int NOT NULL,
+	PRIMARY KEY (teacher_id, class_id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id),
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+
+CREATE TABLE student_class (
+	student_id int NOT NULL,
+    class_id int NOT NULL,
+    grades float,
+    Primary Key (student_id, class_id),
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    FOREIGN KEY (class_id) REFERENCES class(class_id)
+);
+
